b9b4cc949741e8218f3ec285f47f8e151ba59a8f
Johan Nyman <johan.nyman97@gmail.com>
2024-01-08 22:18:49 +0100

    övning 1-3

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace datastrukturer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        // Övning1();
+        Övning3();
+        static void Övning1() 
+        {
+            List<string> lista = new List<string>();
+            lista.Add("hej");
+            lista.Add("hejsan");
+            lista.Add("hallå");
+
+            foreach(string word in lista)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        static void Övning3() 
+        {
+           string? input = Console.ReadLine();
+           string[] inputArr = input.Split(" ");
+           List<int> resultList = new List<int>();
+           foreach(string numberStr in inputArr) {
+             if (int.TryParse(numberStr, out int number))
+                {
+                    if (number % 2 == 0)
+                    {
+                        resultList.Add(number);
+                    }
+                }
+           }
+           string resultString = string.Join(", ", resultList);
+
+            Console.WriteLine($"Jämna nummer i listan: {resultString}");
+        }
+    }
+}
